76f8ca36e67a85ecb4ef63dc52eed57c82c017b6
TakumiNomura <ctuf1024@mail4.doshisha.ac.jp>
2020-07-16 14:01:47 +0900

    Fixed

--- hello/hello.go
+++ hello/hello.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	file, _ := os.Open("C:/Users/bat/go/src/github.com/Bats_Swarming_Robot/hello/data.txt")
+	file, _ := os.Open("C:/Users/bat/go/src/github.com/Nomura_test/hello/data.txt")
 	defer file.Close()
 	data := make([]byte, 100)
 	file.Read(data)
